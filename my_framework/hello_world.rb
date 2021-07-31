2bfa1c743fddaccf9084e8b663532fbb845224d3
Andrew Moore <andrewdmoore84@gmail.com>
2021-07-31 19:51:06 -0400

    Add /advice page and 404 error response

--- my_framework/hello_world.rb
+++ my_framework/hello_world.rb
@@ -1,7 +1,21 @@
 # hello_world.rb
 
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {"Content-Type" => 'text/plain'}, ["Hello World!"]]
+    when '/advice'
+      piece_of_advice = Advice.new.generate    # random piece of advice
+      ['200', {"Content-Type" => 'text/plain'}, [piece_of_advice]]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/plain', "Content-Length" => '13'},
+        ["404 Not Found"]
+      ]
+    end
   end
 end
