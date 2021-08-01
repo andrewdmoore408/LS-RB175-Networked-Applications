31edd98d759889e9f7ccd722f8b82a934bea88c6
Andrew Moore <andrewdmoore84@gmail.com>
2021-08-01 11:27:04 -0400

    Add dynamic routing for chapter URLs and remove unnecessary code

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -3,10 +3,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  @title = "Ruby Toozdei"
-  @chapters = File.readlines("data/toc.txt").map do |chapter|
-    chapter.gsub(/\n/, "")
-  end
-  # @chapters = @chapters.map { |chapter| chapter.gsub(/\n/, "") }
+  @title = "The Adventures of Sherlock Holmes"
+  @chapter_names = File.readlines("data/toc.txt")
+
   erb :home
+end
+
+get "/chapters/:chapter_num" do
+  @title = "The Adventures of Sherlock Holmes"
+  @chapter_names = File.readlines("data/toc.txt")
+
+  @chapter_num = params["chapter_num"]
+  @chapter_text = File.read("data/chp#{@chapter_num}.txt")
+
+  erb :chapter
 end
