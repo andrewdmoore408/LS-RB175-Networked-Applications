4f34ff837eaaa0858eb7d903df734e8c8e484e54
Andrew Moore <andrewdmoore84@gmail.com>
2021-08-02 11:23:03 -0400

    Extract array/string processing of chapter text into a helper method which the view calls

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -3,18 +3,28 @@ require "sinatra"
 require "sinatra/reloader"
 
 before do
-  @title = "The Adventures of Sherlock Holmes"
   @chapter_names = File.readlines("data/toc.txt")
 end
 
+helpers do
+  def in_paragraphs(arr_text)
+    arr_text.map do |graph|
+      "<p>#{graph.gsub(/\n/, " ")}</p>"
+    end.join
+  end
+end
+
 get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
   erb :home
 end
 
 get "/chapters/:chapter_num" do
-  @chapter_num = params["chapter_num"]
+  @chapter_num = params["chapter_num"].to_i
+  @title = "#{@chapter_num}: #{@chapter_names[@chapter_num - 1]}"
+
   @raw_chapter = File.readlines("data/chp#{@chapter_num}.txt", "\n\n")
-  @chapter_text = @raw_chapter.map { |graph| graph.gsub(/\n/, " ") }
 
   erb :chapter
 end
